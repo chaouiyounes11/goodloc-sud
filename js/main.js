9aa50ac13959a4e66c8f898030505ea8ed054fbb
Younes Chaoui <chaouiyounes11@gmail.com>
2018-06-12 15:06:30 +0200

    improving design mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 
-
-
 $(function() {
     var header = $(".bg-light");
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        if (scroll >= 90) {
+        if (scroll >= 50) {
             header.addClass("scrolled");
         } else {
             header.removeClass("scrolled");
