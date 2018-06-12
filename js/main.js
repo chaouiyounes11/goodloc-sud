00a4b152c58c07ab2d686e4d9be47ac962028180
Younes Chaoui <chaouiyounes11@gmail.com>
2018-06-12 18:12:43 +0200

    version mobile done

--- js/main.js
+++ js/main.js
@@ -12,3 +12,10 @@ $(function() {
     });
 
 });
+
+function initMap()
+{ var frejus = {lat: 43.425190, lng: 6.768370
+};
+ var map = new google.maps.Map(document.getElementById('map'), { zoom: 13, center: frejus
+ });
+ var marker = new google.maps.Marker({ position: frejus, map: map }); }
