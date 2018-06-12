e800d9ce384e18c08b1efcd966915bc581ed1bd0
Younes Chaoui <chaouiyounes11@gmail.com>
2018-06-12 13:55:48 +0200

    advanced mobile version

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+$(function() {
+    var header = $(".bg-light");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+
+});
